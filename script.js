4e181030bdf9bc268a4ac268f5b0946f865c290d
kelvin_arcanjo0 <kelvinarcanjonzinga@gmail.com>
2026-07-04 18:01:16 +0100

    feat: complete dynamic modal handling and transaction submission pipeline (Milestone 2)
    - Linked the transaction form submission with an event listener to extract modal inputs.
    - Applied clean code naming conventions by removing repetitive 'Input' suffixes from variables.
    - Implemented an intermediate step to parse input text into decimal numbers using parseFloat().
    - Integrated successful data dispatch to 'adicionarTransacao()', triggering state update and storage persistence.
    - Added UI reactive behaviors to close the dialog container and reset form values upon submission.

--- script.js
+++ script.js
@@ -23,3 +23,43 @@ function adicionarTransacao(tipo , descricao , valor , data ,  categoria){
 
     localStorage.setItem('transacoes' , JSON.stringify(transacoes))
 }
+
+//Evento para o Dialog;
+
+const btn_newTransaction = document.getElementById('newTransaction')
+
+btn_newTransaction.addEventListener('click' , ()=>{
+    document.getElementById('transactionModal').showModal()
+})
+
+//Ligar o formulário do modal a esta função;
+
+const form = document.getElementById('transactionForm')
+
+form.addEventListener('submit' , (event)=>{
+    event.preventDefault()
+
+    //Capturar e Ler os valores dos 5 campos do formulário,
+
+    const tipoInput = document.getElementById('modalType').value
+    const descricaoInput = document.getElementById('modalDescription').value
+
+    let valorString = document.getElementById('modalAmount').value
+    const valorInput = parseFloat(valorString)
+
+    const dataInput = document.getElementById('modalDate').value
+    const categoriaInput = document.getElementById('modalCategory').value
+
+    //Chamar adicionarTransacao(...);
+
+    adicionarTransacao(tipoInput , descricaoInput , valorInput , dataInput , categoriaInput)
+
+    //Fechar o modal;
+
+    document.getElementById('transactionModal').close()
+
+    //Limpa o formulário para a próxima abertura;
+
+    form.reset()
+
+})
